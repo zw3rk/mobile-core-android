b610087138d7a3adbd02ae9ec55aa9266d3576c6
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-25 15:06:54 +0800

    initial checkin

---
+++ app/src/main/java/com/zw3rk/mobile_core_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zw3rk.mobile_core_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
