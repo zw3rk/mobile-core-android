621f56119573c32861b332bb97b2ac936e87a690
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-26 14:40:09 +0800

    add textview logic

--- app/src/main/cpp/mobile_core_android.cpp
+++ app/src/main/cpp/mobile_core_android.cpp
@@ -19,9 +19,15 @@
 //    }
 
 extern "C" void hs_init(int * argc, char **argv[]);
+extern "C" char *hello(void);
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_zw3rk_mobile_1core_1android_MainActivityKt_initHS(JNIEnv *env, jclass clazz) {
     hs_init(NULL, NULL);
+}
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_zw3rk_mobile_1core_1android_MainActivityKt_hello(JNIEnv *env, jclass clazz) {
+    return env->NewStringUTF(hello());
 }
--- app/src/main/java/com/zw3rk/mobile_core_android/MainActivity.kt
+++ app/src/main/java/com/zw3rk/mobile_core_android/MainActivity.kt
@@ -2,13 +2,17 @@ package com.zw3rk.mobile_core_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 external fun initHS()
+external fun hello() : String
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.example)
+        tv.text = hello()
     }
     companion object {
         init {
